b1daa22ac20957715dbe095f67acdab37ce4fc34
raoul grouls <Raoul.Grouls@han.nl>
2025-06-14 18:42:55 +0200

    index.html test file

--- src/swarmcraft/main.py
+++ src/swarmcraft/main.py
@@ -48,6 +48,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
+        "http://localhost:8001",
     ],
     allow_credentials=True,
     allow_methods=["*"],
